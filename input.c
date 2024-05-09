8dfd92c7927637d8f881dc1d49cd9097f8dcf46e
Spectre114 <hmtv2003@gmail.com>
2024-05-09 14:56:49 +0530

    Latest Commit

---
+++ input.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    /*Hello world*/
+    //Hello World
+    printf("Hello World");
+    return 0;
+}
+
--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -362,17 +362,17 @@ struct yy_trans_info
 	};
 static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    4,    3,    1,    0,    1,    2,    0
+        0,    0,    5,    3,    4,    2,    1,    3,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    2,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,33 +397,31 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1,    2
+        1,    2,    2,    2
     } ;
 
-static const flex_int16_t yy_base[12] =
+static const flex_int16_t yy_base[11] =
     {   0,
-        6,    5,    7,   10,    0,    0,    0,    0,   10,    3,
-        4
+        0,    0,    6,    0,    7,    7,    7,    0,    7,    4
     } ;
 
-static const flex_int16_t yy_def[12] =
+static const flex_int16_t yy_def[11] =
     {   0,
-       10,   10,    9,    9,    9,   11,    5,   11,    0,    9,
+        9,    1,    9,   10,    9,    9,    9,   10,    0,    9
+    } ;
+
+static const flex_int16_t yy_nxt[12] =
+    {   0,
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
         9
     } ;
 
-static const flex_int16_t yy_nxt[14] =
+static const flex_int16_t yy_chk[12] =
     {   0,
-        9,    6,    7,    4,    4,    8,    9,    5,    5,    3,
-        9,    9,    9
-    } ;
-
-static const flex_int16_t yy_chk[14] =
-    {   0,
-        0,    5,    5,   10,   10,   11,    3,    2,    1,    9,
-        9,    9,    9
+        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
+        9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -440,13 +438,17 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "fileHandle.l"
-#line 2 "fileHandle.l"
+#line 1 "fileHandle2.l"
+#line 2 "fileHandle2.l"
     #include <stdio.h>
     #include <stdlib.h>
     #include <string.h>
-#line 448 "lex.yy.c"
-#line 449 "lex.yy.c"
+    int newLine = 0;
+    int space = 0;
+    int c = 0;
+    int words = 0;
+#line 450 "lex.yy.c"
+#line 451 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -663,9 +665,9 @@ YY_DECL
 		}
 
 	{
-#line 6 "fileHandle.l"
+#line 10 "fileHandle2.l"
 
-#line 668 "lex.yy.c"
+#line 670 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -698,7 +700,7 @@ yy_match:
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 10 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -724,24 +726,26 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "fileHandle.l"
-{
-    fprintf(yyout,"Integer: %s\n",yytext);
-}
+#line 11 "fileHandle2.l"
+{space++;}
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 10 "fileHandle.l"
-{
-    fprintf(yyout,"Float: %s",yytext);
-}
+#line 12 "fileHandle2.l"
+{newLine++;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 13 "fileHandle.l"
+#line 13 "fileHandle2.l"
+{words++;c += yyleng;}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 14 "fileHandle2.l"
 ECHO;
 	YY_BREAK
-#line 744 "lex.yy.c"
+#line 748 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1746,7 +1750,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 13 "fileHandle.l"
+#line 14 "fileHandle2.l"
 
 int yywrap() {
     return 1;
@@ -1756,6 +1760,12 @@ int main(){
     yyin=fopen("input.txt","r");
     yyout=fopen("output.txt","w");
     yylex();
+    fprintf(yyout,"No. of chars: %d\n",c);
+    fprintf(yyout,"No. of spaces: %d\n",space);
+    fprintf(yyout,"No. of new lines: %d\n",newLine);
+    fprintf(yyout,"No. of words: %d\n",words);
+    fclose(yyin);
+    fclose(yyout);
     return 0;
 }
 
