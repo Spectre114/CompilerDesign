070b4ca709dff8418d57b20dbf9cfc71924d2ca4
Spectre114 <hmtv2003@gmail.com>
2024-05-16 14:01:05 +0530

    16-May-2024

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,20 +360,20 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[11] =
     {   0,
-        0,    0,    5,    3,    4,    2,    1,    3,    0
+        0,    0,    4,    2,    3,    1,    2,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    4,
+        1,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,31 +397,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[6] =
     {   0,
-        1,    2,    2,    2
+        1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[12] =
     {   0,
-        0,    0,    6,    0,    7,    7,    7,    0,    7,    4
+        0,    0,    9,   10,   10,   10,    3,    2,   10,   10,
+        5
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[12] =
     {   0,
-        9,    1,    9,   10,    9,    9,    9,   10,    0,    9
+       10,    1,   10,   10,   10,   10,   11,   11,   10,    0,
+       10
     } ;
 
-static const flex_int16_t yy_nxt[12] =
+static const flex_int16_t yy_nxt[16] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
-        9
+        4,    5,    6,    7,    4,    8,    9,    9,   10,    3,
+       10,   10,   10,   10,   10
     } ;
 
-static const flex_int16_t yy_chk[12] =
+static const flex_int16_t yy_chk[16] =
     {   0,
-        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
-        9
+        1,    1,    1,    1,    1,   11,    8,    7,    3,   10,
+       10,   10,   10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -438,17 +440,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "fileHandle2.l"
-#line 2 "fileHandle2.l"
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
-    int newLine = 0;
-    int space = 0;
-    int c = 0;
-    int words = 0;
-#line 450 "lex.yy.c"
-#line 451 "lex.yy.c"
+#line 1 "htmlTag.l"
+#line 2 "htmlTag.l"
+    #include<stdio.h>
+    #include<stdlib.h>
+#line 447 "lex.yy.c"
+#line 448 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -665,9 +662,9 @@ YY_DECL
 		}
 
 	{
-#line 10 "fileHandle2.l"
+#line 5 "htmlTag.l"
 
-#line 670 "lex.yy.c"
+#line 667 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -694,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 10 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -725,27 +722,22 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 11 "fileHandle2.l"
-{space++;}
+#line 6 "htmlTag.l"
+{fprintf(yyout,"%s",yytext);}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 12 "fileHandle2.l"
-{newLine++;}
+#line 7 "htmlTag.l"
+;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 13 "fileHandle2.l"
-{words++;c += yyleng;}
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 14 "fileHandle2.l"
+#line 8 "htmlTag.l"
 ECHO;
 	YY_BREAK
-#line 748 "lex.yy.c"
+#line 740 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1042,7 +1034,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1070,11 +1062,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 10);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1750,23 +1742,16 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 14 "fileHandle2.l"
+#line 8 "htmlTag.l"
 
-int yywrap() {
+int yywrap(){
     return 1;
 }
 int main(){
     extern FILE *yyin,*yyout;
-    yyin=fopen("input.txt","r");
-    yyout=fopen("output.txt","w");
+    yyin = fopen("htmlFile.html","r");
+    yyout = fopen("htmlTags.txt","w");
     yylex();
-    fprintf(yyout,"No. of chars: %d\n",c);
-    fprintf(yyout,"No. of spaces: %d\n",space);
-    fprintf(yyout,"No. of new lines: %d\n",newLine);
-    fprintf(yyout,"No. of words: %d\n",words);
     fclose(yyin);
     fclose(yyout);
-    return 0;
 }
-
-
